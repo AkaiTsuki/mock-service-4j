b5b9f93ce16e920c857e2a52ef6da2f78081586f
Jiachi Liu <liu.jiach@husky.neu.edu>
2015-12-21 11:39:33 -0500

    add servlet mapping for different components add simple controller to return hello information

---
+++ src/main/java/com/hazelnut/mockservice/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.hazelnut.mockservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by jiachiliu on 12/21/15.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public
+    @ResponseBody
+    String hello() {
+        return "Hello service";
+    }
+}
